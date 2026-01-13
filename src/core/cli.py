3cc566784b36e3bfebb1a48c288eaefa3a43c392
raziel <tvindzon@gmail.com>
2026-01-13 22:16:08 +0700

    update cli.py'

--- src/core/cli.py
+++ src/core/cli.py
@@ -22,7 +22,7 @@ def main() -> None:
 
     if len(list_sys_argv) < 2:
         print(
-            "Использование:\n\npython manage.py add-module <путь>\npython manage.py remove-module <путь>"
+            "Использование:\n\ncli add-module <путь> <путь> - Создание модулей\ncli remove-module <путь> - Удаление модуля"
         )
         sys.exit()
 
@@ -31,7 +31,7 @@ def main() -> None:
         if len(list_sys_argv) < 3:
             print(
                 "Укажите путь до модуля.Дочерний модуль должен быть "
-                "разделен 'childes'\ncli add-module <путь>"
+                "разделен 'childes'\ncli add-module <путь> <путь> - Создание модулей"
                 "\ncli add-module test/childes/data"
             )
             exit()
@@ -46,8 +46,8 @@ def main() -> None:
         if len(list_sys_argv) < 3:
             print(
                 "Укажите путь до модуля.Дочерний модуль должен быть "
-                "разделен 'childes'\ncli add-module <путь>"
-                "\ncli add-module test/childes/data"
+                "разделен 'childes'\ncli remove-module <путь>"
+                "\nli remove-module test/childes/data"
             )
             exit()
         elif len(list_sys_argv) >= 4:
